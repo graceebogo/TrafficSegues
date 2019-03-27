b462a04a693b70263df2485232cdbca924e2ffb7
Soft Dev Student <softdevstudent@robotics-mba-10.local>
2019-03-27 14:15:29 -0400

    Initial Commit

---
+++ TrafficSegues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrafficSegues
+//
+//  Created by Soft Dev Student on 3/27/19.
+//  Copyright © 2019 Soft Dev Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
